663abd1e10254d490e51b8d644493c5316f90449
EstebanJuarez <esteban.juarez0011@gmail.com>
2024-11-13 01:43:09 -0300

    me obligan a hacer esto, supuestamente algo de db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('fullservice','root','database@',{
+const db = new Sequelize('fullservice','root','QWEasd123!',{
     host:'127.0.0.1',
     dialect:'mysql',
     pool: {
